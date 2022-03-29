58a69a0c54aa26b98b6a4acfedbd01467ab18f61
emerson.yur <emerson.yur@gmail.com>
2022-03-29 09:53:36 +0200

    Configure Postgres Database with Docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ContactsModule } from './Contacts/contacts.module';
-// import { DatabaseModule } from './core/database/database.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { DatabaseModule } from './core/database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ContactsModule,
-    // DatabaseModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/core/constants/index.ts
@@ -0,0 +1,5 @@
+export const SEQUELIZE = 'SEQUELIZE';
+export const DEVELOPMENT = 'development';
+export const TEST = 'test';
+export const PRODUCTION = 'production';
+export const CONTACT_REPOSITORY = 'CONTACT_REPOSITORY';

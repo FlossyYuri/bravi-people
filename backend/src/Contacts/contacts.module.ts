5f4b2b2c5cc68b9c1d4f99e4cdf7e200acc0904f
emerson.yur <emerson.yur@gmail.com>
2022-03-30 21:13:18 +0200

    Add Sequelize

--- backend/src/Contacts/contacts.module.ts
+++ backend/src/Contacts/contacts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/core/database/database.module';
 import { contactsProviders } from './contact.provider';
 import { ContactsController } from './contacts.controller';
 import { ContactsService } from './contacts.service';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ContactsController],
   providers: [ContactsService, ...contactsProviders],
 })
